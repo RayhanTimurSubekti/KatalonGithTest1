aa59ee71820afd1453778a03b287c462cab5afa6
rayhantimursubekti <rayhantimursubekti@172.18.1.85>
2018-08-06 18:55:14 +0700

    added smoke test

---
+++ Test Suites/SmokeTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SmokeTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-06T18:47:30</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>41f8aad6-9640-4f6e-9d12-1d21e3c61f0a</testSuiteGuid>
+   <testCaseLink>
+      <guid>72707bd1-3313-4e53-844d-bfe4b252076f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginTest1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
